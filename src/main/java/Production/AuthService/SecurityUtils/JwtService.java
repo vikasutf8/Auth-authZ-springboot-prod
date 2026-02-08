0fb40b6e89a0590e7856be89a93aa9bb3d97c345
Vikas Arya <117626243+vikasutf8@users.noreply.github.com>
2026-02-08 16:18:52 +0530

    config for jwt

---
+++ src/main/java/Production/AuthService/SecurityUtils/JwtService.java
@@ -0,0 +1,12 @@
+package Production.AuthService.SecurityUtils;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JwtService {
+
+
+}
